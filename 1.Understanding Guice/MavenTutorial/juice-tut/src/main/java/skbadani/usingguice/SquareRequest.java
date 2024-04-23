c7c94608e9a57c737488cf80feca3b1e1ae28b57
Shaunak Badani <shaunak.badani@research.iiit.ac.in>
2024-04-24 00:49:07 +0530

    Field level injection

--- 1.Understanding Guice/MavenTutorial/juice-tut/src/main/java/skbadani/usingguice/SquareRequest.java
+++ 1.Understanding Guice/MavenTutorial/juice-tut/src/main/java/skbadani/usingguice/SquareRequest.java
@@ -3,19 +3,14 @@ package skbadani.usingguice;
 import com.google.inject.Inject;
 
 public class SquareRequest {
+    @Inject
     DrawShape d;
 
     public DrawShape getDrawShape()
     {
         return d;
     }
-
-    @Inject
-    public void setDrawShape(DrawShape d)
-    {
-        System.out.println("Set draw shape called");
-        this.d = d;
-    }
+ 
 
     public void makeRequest()
     {
